eed1a345ed53ebb515ed509bede0e5a64c53ae40
Sean Erfurt <serfurt1@jhu.edu>
2017-12-12 02:37:44 -0600

    Cleaned up windows version to assume scripts have been added to Path env var

--- bedtime/Windows/Sleep_in_ten.bat
+++ bedtime/Windows/Sleep_in_ten.bat
@@ -10,12 +10,12 @@ start chrome "https://www.youtube.com/watch?v=-zJfwr-SZgY"
 for /f "TOKENS=1" %%a in ('wmic PROCESS where "Name='chrome.exe'" get ProcessID ^| findstr [0-9]') do (
 if "!oldPIDlist:p%%ap=zz!"=="%oldPIDlist%" (set "newPIDlist=/PID %%a !newPIDlist!")
 )
-echo %newPIDlist%
+::echo %newPIDlist%
 ::Initiate 10min countdown to sleep mode
-C:\WINDOWS\system32\ps_tools\psshutdown.exe -d -t 600
+psshutdown -d -t 600
 ::call PS script to dim brightness
 ::Note security work-around of starting an inner PowerShell process as Administrator (impossible from within cmd.exe)
-powershell -executionPolicy bypass -command "& {Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -Command ""C:\Users\dragu_000\Documents\Misc\brightness_control.ps1 -Dim""' -Verb RunAs}";
+powershell -executionPolicy bypass -command "& {Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -Command ""brightness_control.ps1 -Dim""' -Verb RunAs}";
 ::kill new tab process 10s before sleep
 timeout /t 590 /nobreak >nul
 taskkill /f %newPIDlist% /T > NUL 2>&1
