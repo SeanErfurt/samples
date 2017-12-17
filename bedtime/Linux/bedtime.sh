4d81b5c64f93174fa0b42fbfed9767f62d0311fa
Sean Erfurt <serfurt1@gmail.com>
2017-12-17 03:47:11 -0600

    changed to firefox due to profile issues; simplified closing with wmctrl

--- bedtime/Linux/bedtime.sh
+++ bedtime/Linux/bedtime.sh
@@ -7,41 +7,25 @@ BRIGHTFILE=`ls /sys/class/backlight/*/brightness`
 # get current brightness
 BRIGHTVAL=`cat $BRIGHTFILE`
 # set to very dim
-echo 1 | sudo tee $BRIGHTFILE
+echo 1 | sudo tee $BRIGHTFILE > /dev/null
 
 # Tell crontab to set brightness back to previous brightness on startup
 # see: https://stackoverflow.com/questions/878600/how-to-create-a-cron-job-using-bash-automatically-without-the-interactive-editor
+SUBSTRING="sudo tee $BRIGHTFILE"
 CRONCMD="@reboot echo $BRIGHTVAL | sudo tee $BRIGHTFILE"
-( crontab -l 2> /dev/null | grep -v -F "$CRONCMD" ; echo "$CRONCMD" ) | crontab -
+( crontab -l 2> /dev/null | grep -v -F "$SUBSTRING" ; echo "$CRONCMD" ) | crontab -
 
-# Get chromium PIDs
-declare -a OLDPIDS=(`pgrep chromium`)
-echo "${OLDPIDS[*]}"
-# open youtube music url
-( chromium-browser --no-sandbox "https://www.youtube.com/watch?v=-zJfwr-SZgY" ) &
-# Get chromium PIDs after new window/tab opens
-sleep 1
-declare -a NEWPIDS=(`pgrep chromium`)
-echo "${NEWPIDS[*]}"
-
-# Find new process id, if any
-declare -a COPYNEW=("${NEWPIDS[@]}")
-for ((i=0; i<${#NEWPIDS[@]}; i++)); do
-	for j in "${OLDPIDS[@]}"; do
-		if [[ ${NEWPIDS[$i]} == $j ]]; then
-			unset COPYNEW[$i]
-			break
-		fi
-	done
-done
+# open music url in $1 (default is youtube relaxing music)
+# using new profile "system" to avoid messing with the default profile's startup
+# will prompt you with profile manager on first run, create new profile named "system"
+if [ "$1" == "" ]; then
+	( firefox --new-window "https://www.youtube.com/watch?v=-zJfwr-SZgY" -P system) &>/dev/null &
+else
+	( firefox --new-window "$1" -P system) &>/dev/null &
+fi
 
 # Power off after ten minutes
 sudo shutdown -P 10 &
-# Wait almost 10 minutes, then attempt to close new chromium window/tab
+# Wait almost 10 minutes, then close new firefox window (assuming user hasn't changed active window)
 sleep 595
-if [ -z "$OLDPIDS" ]; then
-	pkill --oldest chromium
-elif [[ ${#COPYNEW[@]} -gt 0 ]]; then
-	kill ${COPYNEW[*]}
-fi
-exit
+wmctrl -c :ACTIVE:
